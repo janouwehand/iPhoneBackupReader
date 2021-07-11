f59fe91201a064790b035a18d8b5c72b84e3c0f1
J.L. Ouwehand <jan@softable.nl>
2021-07-11 19:47:59 +0200

    Initial

---
+++ Proj/FormMain.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace iPhoneBackupReader
+{
+  partial class FormMain
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.buttonDoStuff = new System.Windows.Forms.Button();
+      this.edt = new System.Windows.Forms.RichTextBox();
+      this.SuspendLayout();
+      // 
+      // buttonDoStuff
+      // 
+      this.buttonDoStuff.Dock = System.Windows.Forms.DockStyle.Bottom;
+      this.buttonDoStuff.Location = new System.Drawing.Point(0, 253);
+      this.buttonDoStuff.Name = "buttonDoStuff";
+      this.buttonDoStuff.Size = new System.Drawing.Size(742, 56);
+      this.buttonDoStuff.TabIndex = 0;
+      this.buttonDoStuff.Text = "Action";
+      this.buttonDoStuff.UseVisualStyleBackColor = true;
+      this.buttonDoStuff.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // edt
+      // 
+      this.edt.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.edt.Location = new System.Drawing.Point(0, 0);
+      this.edt.Name = "edt";
+      this.edt.Size = new System.Drawing.Size(742, 253);
+      this.edt.TabIndex = 1;
+      this.edt.Text = "";
+      // 
+      // FormMain
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(742, 309);
+      this.Controls.Add(this.edt);
+      this.Controls.Add(this.buttonDoStuff);
+      this.Name = "FormMain";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "iPhone Backup reader";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button buttonDoStuff;
+    private System.Windows.Forms.RichTextBox edt;
+  }
+}
+
---
+++ Proj/FormMain.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace iPhoneBackupReader
+{
+  public partial class FormMain : Form
+  {
+    private string folderSource, manifestFile, folderDestination;
+    private string[] extensions;
+
+    public FormMain() => InitializeComponent();
+
+    protected override void OnShown(EventArgs e)
+    {
+      folderSource = ConfigurationManager.AppSettings["BackupFolder"].TrimEnd('\\') + '\\';
+      manifestFile = string.Concat(folderSource, "Manifest.db");
+      folderDestination = ConfigurationManager.AppSettings["DestinationFolder"].TrimEnd('\\') + '\\';
+      extensions = ConfigurationManager.AppSettings["FindFileExtensions"].Split(',');
+
+      edt.AppendText($@"Source folder: {folderSource}
+Destination folder: {folderDestination}
+Manifest file: {manifestFile}
+Find files: {(string.Join(", ", extensions))}");
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      using (var db = new SQLiteConnection(manifestFile))
+      {
+        db.Open();
+
+        ReadFilesFromDB(db);
+        ReadFilesFromFolder();
+        Execute();
+      }
+    }
+
+    private class FileRec
+    {
+      public string ID { get; set; }
+      public string FilePathName { get; set; }
+    }
+
+    private readonly Dictionary<string, FileRec> filesdb = new Dictionary<string, FileRec>();
+    private Dictionary<string, string> filesdisc = new Dictionary<string, string>();
+
+    private void ReadFilesFromDB(SQLiteConnection db)
+    {
+      using (var cmd = db.CreateCommand())
+      {
+        cmd.CommandText = @"SELECT * from Files;";
+        using (var rdr = cmd.ExecuteReader())
+        {
+          while (rdr.Read())
+          {
+            var id = rdr[0].ToString();
+            var pth = rdr[2].ToString();
+            filesdb.Add(id, new FileRec { ID = id, FilePathName = pth });
+          }
+        }
+      }
+    }
+
+    private void ReadFilesFromFolder()
+    {
+      var files = Directory.GetFiles(folderSource, "*.*", SearchOption.AllDirectories);
+
+      filesdisc = files
+        .GroupBy(x => Path.GetFileName(x))
+        .ToDictionary(x => x.Key, x => x.First());
+    }
+
+    private void Execute()
+    {
+      foreach (var fn in filesdisc)
+      {
+        if (!filesdb.TryGetValue(fn.Key, out var rec))
+        {
+          continue;
+        }
+
+        if (rec.FilePathName == null || rec.FilePathName.Length < 5)
+        {
+          continue;
+        }
+
+        var ext = rec.FilePathName.Substring(rec.FilePathName.Length - 4).ToLower();
+
+        if (!extensions.Contains(ext))
+        {
+          continue;
+        }
+
+        CopyFile(fn.Value, rec);
+      }
+    }
+
+    private void CopyFile(string sourceFile, FileRec rec)
+    {
+      //Media/DCIM/114APPLE/IMG_4792.JPG
+
+      var destFile = string.Concat(
+        folderDestination.TrimEnd('\\'),
+        '\\',
+        rec.FilePathName.Replace('/', '\\'));
+
+      if (rec.FilePathName.ToLower().Contains("whatsapp"))
+      {
+        destFile = string.Concat(
+          folderDestination.TrimEnd('\\'),
+          '\\',
+          "WhatsApp\\",
+          Path.GetFileName(rec.FilePathName));
+      }
+      else if (rec.FilePathName.ToLower().Contains("@g.us"))
+      {
+        destFile = string.Concat(
+          folderDestination.TrimEnd('\\'),
+          '\\',
+          "Google\\",
+          Path.GetFileName(rec.FilePathName));
+      }
+
+      var destMap = Path.GetDirectoryName(destFile);
+
+      if (!Directory.Exists(destMap))
+      {
+        Directory.CreateDirectory(destMap);
+      }
+
+      File.Copy(sourceFile, destFile);
+    }
+
+  }
+}
